d145f68111586aee5eb7658af19aeba5beaed929
ka28-dec <60833391+ka28-dec@users.noreply.github.com>
2024-07-11 11:24:20 +0900

    ver 2.0

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def str_to_wav(text):
 
 
 if __name__ == "__main__":
-    pdf = pdfium.PdfDocument("94081601_01.pdf")
+    pdf = pdfium.PdfDocument("94081601_01.pdf")  # 読み込みたいPDFのパス
 
     total_pages = len(pdf)
     print(f"全{total_pages}ページ 処理開始")
